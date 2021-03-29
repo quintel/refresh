86251a6c1635194aac60eb23b82580f666f0ff58
Anthony Williams <hi@antw.dev>
2021-03-29 16:55:15 +0100

    Add styled components with Emotion.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+const cache = createCache({ prepend: true, key: 'css' });
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const decorators = [
+  (Story) => (
+    <CacheProvider value={cache}>
+      <Story />
+    </CacheProvider>
+  ),
+];
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,12 @@
-interface ButtonProps {
+import styled from '@emotion/styled';
+
+export interface ButtonProps {
   children: React.ReactNode;
 }
 
-export default function Button({ children }: ButtonProps): JSX.Element {
-  return <button>{children}</button>;
-}
+export default styled.button`
+  background: hsl(222deg 62% 60%);
+  border: 0;
+  color: #fff;
+  padding: 0.5rem 0.75rem;
+`;
