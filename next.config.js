68b981ea9bcdf1cd7f5195f86f3ddde23220a9e2
Anthony Williams <hi@antw.dev>
2021-05-18 15:00:07 +0100

    Update Next to 10.2

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
-    config.plugins.push(new TsconfigPathsPlugin({}));
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
 
     // Important: return the modified config
     return config;
