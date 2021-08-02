d2d493b350479504f358bc2f4de9c95cf65b6c40
Anthony Williams <hi@antw.dev>
2021-08-02 15:54:53 +0100

    Series and annotations have unique names in tests

--- src/components/chart/helpers/testHelpers.ts
+++ src/components/chart/helpers/testHelpers.ts
@@ -1,10 +1,21 @@
 import { AnnotationConfig, ChartConfig, SeriesConfig } from '../types';
 
+// Used to generate unique IDs for series and annotations. Not thread-safe, but it only matters that
+// numbers are monotonic within a single test.
+let id = 0;
+
 /**
  * Builds an annotation which may be used in tests.
  */
 export function buildAnnotation(attrs: Partial<AnnotationConfig> = {}): AnnotationConfig {
-  return { color: 'black', name: 'My annotation', type: 'target-line', value: 0, x: 0, ...attrs };
+  return {
+    color: 'black',
+    name: `Annotation ${id++}`,
+    type: 'target-line',
+    value: 0,
+    x: 0,
+    ...attrs,
+  };
 }
 
 /**
@@ -18,5 +29,5 @@ export function buildChart(attrs: Partial<ChartConfig> = {}): ChartConfig {
  * Builds a series which may be used in tests.
  */
 export function buildSeries(attrs: Partial<SeriesConfig> = {}): SeriesConfig {
-  return { color: 'black', name: 'My series', value: [], ...attrs };
+  return { color: 'black', name: `Series ${id++}`, value: [], ...attrs };
 }
